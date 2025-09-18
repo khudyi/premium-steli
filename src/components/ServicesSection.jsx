9033319f15bbb5125982452aacc097c59b6beaa4
Ihor <ikhudyi@gmail.com>
2025-09-18 19:09:27 +0300

    change ServiceSection title

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -69,7 +69,7 @@ const ServicesSection = () => {
               {service.popular && (
                 <div className="absolute -top-4 left-1/2 transform -translate-x-1/2">
                   <span className="bg-blue-500 text-white px-4 py-2 rounded-full text-sm font-bold">
-                    Найбільш популярний
+                    Popular
                   </span>
                 </div>
               )}
