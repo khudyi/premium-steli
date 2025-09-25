e0f0f738d3c719b2e1ff5ddf146d69b9a5fedd61
Ihor <ikhudyi@gmail.com>
2025-09-25 12:47:36 +0300

    add animation routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
 import Gallery from './pages/Gallery';
@@ -6,17 +7,39 @@ import Contacts from './pages/Contacts';
 import AdminPanel from './pages/AdminPanel';
 import './App.css';
 
+function PageWrapper({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.4, ease: "easeInOut" }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><Homepage /></PageWrapper>} />
+        <Route path="/gallery" element={<PageWrapper><Gallery /></PageWrapper>} />
+        <Route path="/contacts" element={<PageWrapper><Contacts /></PageWrapper>} />
+        <Route path="/admin" element={<PageWrapper><AdminPanel /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/admin" element={<AdminPanel />} />
-        </Routes>
+        <AnimatedRoutes />
       </div>
     </Router>
   );
